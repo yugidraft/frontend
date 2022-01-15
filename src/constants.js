c043248a1961d594b90d665240d678ad1f5f86a5
asood92 <392075+asood92@users.noreply.github.com>
2022-01-14 19:30:41 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,3 @@
-export const SERVER_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3001"
-    : "https://yugidraft.herokuapp.com/";
-
-export const CLIENT_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://yugidraft.herokuapp.com/";
+export const SERVER_URL = "https://yugidraft.herokuapp.com/"
 
 export const MAX_PLAYERS = 8;
